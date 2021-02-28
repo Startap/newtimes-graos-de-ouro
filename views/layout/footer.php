2f660f9acc7b1e7384b3ee5d703e2ded6c782b39
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-02-28 19:47:07 -0300

    ⚙  Fix JS link statement

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,6 +1,6 @@
 <footer>
     Copyright <?php echo date('Y'); ?> | Todos os direitos reservados
 </footer>
-<script type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="assets/app.js"></script>
 </body>
 </html>
