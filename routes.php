2a4fcaa04838400d436ec74567672063f09f0ea7
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-01 11:01:22 -0300

    🚫 Delete "Thank You" route from application

--- routes.php
+++ routes.php
@@ -5,4 +5,3 @@ use Components\Application;
 
 /** Call methods inside PageController when hits some path mapped on argument 1 */
 Router::route('/', '\Components\Controllers\PageController@getHomepage');
-Router::route('/obrigado', '\Components\Controllers\PageController@getAfterSubscription');
