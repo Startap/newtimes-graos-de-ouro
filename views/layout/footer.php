1b16e0344450b32022ef3964c960715ee337b951
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-03 15:06:23 -0300

    Add animations to some icons

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div id="footer-social-media" class="flex flex-row">
-        <a href="https://instagram.com/office_agro" target="_blank" rel="nofollow"><img src="/assets/images/social/instagram.png" alt="Ícone do Instagram" title=""/></a>
-        <a href="https://www.youtube.com/channel/UCet_MLo5hRPt4FuKDhRX2KA" target="_blank" rel="nofollow"><img src="/assets/images/social/youtube.png" alt="Ícone do YouTube" title=""/></a>
-        <a href="htps://t.me/officeagro" target="_blank" rel="nofollow"><img src="/assets/images/social/telegram.png" alt="Ícone do Telegram" title=""/></a>
+        <a class="social-media-icon" href="https://instagram.com/office_agro" target="_blank" rel="nofollow"><img src="/assets/images/social/instagram.png" alt="Ícone do Instagram" title=""/></a>
+        <a class="social-media-icon" href="https://www.youtube.com/channel/UCet_MLo5hRPt4FuKDhRX2KA" target="_blank" rel="nofollow"><img src="/assets/images/social/youtube.png" alt="Ícone do YouTube" title=""/></a>
+        <a class="social-media-icon" href="htps://t.me/officeagro" target="_blank" rel="nofollow"><img src="/assets/images/social/telegram.png" alt="Ícone do Telegram" title=""/></a>
     </div>
 
     <div class="flex flex-justify-content-center flex-row" style="margin-bottom: 60px;">
