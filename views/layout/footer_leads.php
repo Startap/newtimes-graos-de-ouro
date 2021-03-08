dcd0755b67c160424ed4496375e0684290f4bac6
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-08 15:39:12 -0300

    🚧 wip - Handle footer, header and routes of webpages

---
+++ views/layout/footer_leads.php
@@ -0,0 +1,6 @@
+<footer>
+    COPYRIGHT 2021 | OfficeAgro
+</footer>
+<script type="text/javascript" src="assets/app.js"></script>
+</body>
+</html>
--- views/layout/header.php
+++ views/layout/header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <meta name="description" content="%APP_DESCRIPTION%">
     <meta name="keywords" content="%APP_KEYWORDS%">
     <meta name="author" content="André Paul Grandsire <https://github.com/andrepg>">
@@ -11,10 +10,17 @@
 
     <title>%APP_TITLE%</title>
 
+    <!-- Mentoria Graos de Ouro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Damion:wght@400;700&display=swap" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&;900display=swap" crossorigin>
-    <link rel="stylesheet" href="assets/reset.css">
-    <link rel="stylesheet" href="assets/app.css">
+
+    <!-- Captura de leads -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ruda:wght@400;700;900&display=swap" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Amaranth:wght@400;700;900&display=swap" crossorigin>
+
+    <!-- Estilos de aplicacao -->
+    <link rel="stylesheet" href="assets/reset.css">    
+    <link rel="stylesheet" href="%applicationStyle%">
 
     <!-- Favicon declarations -->
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
