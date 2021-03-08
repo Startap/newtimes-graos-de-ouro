738ff40e8bfb0db9b0e3f1dd306df06d472c6d6a
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-03-08 15:38:43 -0300

    🚧 wip - Implement controller methods to respond to webpages

--- components/Controllers/PageController.php
+++ components/Controllers/PageController.php
@@ -5,8 +5,16 @@ namespace Components\Controllers;
 class PageController extends ApplicationController
 { 
 
+    public function getHomepage() {
+        $this->renderViewport(['website/homepage']);
+    }
+
+    public function getObrigado() {
+        $this->renderViewport(['website/obrigado']);
+    }
+
     public function getMentoria()
     {
-        $this->renderViewport(['website/homepage']);
+        $this->renderViewport(['website/mentoria']);
     }
 }
