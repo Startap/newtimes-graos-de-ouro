6ce1851ac2895b90804f5df6d77cf138a7e5e250
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-05-05 10:36:48 -0300

    🧩 Include new Treinamento route

--- components/Controllers/PageController.php
+++ components/Controllers/PageController.php
@@ -25,4 +25,18 @@ class PageController extends ApplicationController
     public function getSemanaAltaProdutividadeObrigado() {
         $this->renderViewport(['semana_alta_produtividade/obrigado']);
     }
+
+    public function getTreinamentoGraosDeOuro() 
+    {
+        $this->renderViewport([
+            'treinamento-graos-de-ouro/headline',
+            'treinamento-graos-de-ouro/testimonial',
+            'treinamento-graos-de-ouro/targets',
+            'treinamento-graos-de-ouro/modules',
+            'website/sections/bonus',
+            'treinamento-graos-de-ouro/investiment',
+            'website/sections/maciel_lemos',
+            'treinamento-graos-de-ouro/faq',
+        ]);
+    }
 }
