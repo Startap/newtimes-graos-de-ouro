403df88aaa9831b2eea942beebd138f0cee7c9bc
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-06-14 17:00:23 -0300

    💌 Include new WhatsApp variable

--- routes.php
+++ routes.php
@@ -4,10 +4,10 @@ use Components\Router;
 use Components\Application;
 
 /** Call methods inside PageController when hits some path mapped on argument 1 */
-Router::route('/', '\Components\Controllers\PageController@getHomepage');
-Router::route('/obrigado', '\Components\Controllers\PageController@getObrigado');
+// Router::route('/obrigado', '\Components\Controllers\PageController@getObrigado');
 Router::route('/mentoria', '\Components\Controllers\PageController@getMentoria');
 
+Router::route('/', '\Components\Controllers\PageController@getHomepage');
 Router::route('/semana-alta-produtividade', '\Components\Controllers\PageController@getSemanaAltaProdutividadeIndex');
 Router::route('/semana-alta-produtividade/obrigado', '\Components\Controllers\PageController@getSemanaAltaProdutividadeObrigado');
 
--- views/semana_alta_produtividade/obrigado.php
+++ views/semana_alta_produtividade/obrigado.php
@@ -1,3 +1,7 @@
+<?php
+$urlWhatsAppGroup = isset($_ENV['WHATSAPP_URL']) ? $_ENV['WHATSAPP_URL'] : 'https://chat.whatsapp.com/F760rbeNtxO8yw2nIVG0gM';
+?>
+
 <section class="section__content" id="topbar__section">
     <article>
         <h1>Tudo certo com sua inscrição</h1>
@@ -10,7 +14,7 @@
         <h3>Clique nos botões abaixos para garantir seu acesso</h3>
 
         <div class="ty-social-network-card-containers">
-            <a href="https://chat.whatsapp.com/F760rbeNtxO8yw2nIVG0gM" target="_blank" rel="norel nofollow" class="whatsapp ty-social-network-card">
+            <a href="<?php echo $urlWhatsAppGroup; ?>" target="_blank" rel="norel nofollow" class="whatsapp ty-social-network-card">
                 <span class="iconify" data-icon="logos:whatsapp" data-inline="false"></span>
                 <span>Participe do nosso grupo no WhatsApp</span>
             </a>
